790f4ded79232de338ff4e2ed74aac6754aa65ab
Vedant Pingale <vedantpingale1@gmail.com>
2024-02-14 14:42:38 +0530

    Amazon Clone, Navbar 70%

---
+++ src/components/MainPage/MainPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import "./MainPage.css"
+class MainPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return (  <>
+        MainPage
+        </>);
+    }
+}
+ 
+export default MainPage;
